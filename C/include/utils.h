ce4a49dd169e13f102e4a429c4d503ab838e39c7
Subht21 <subhash@lokawiz.com>
2016-02-11 22:32:40 +0530

    Add fresh util.h

---
+++ C/include/utils.h
@@ -0,0 +1,19 @@
+/*
+ * utils.h
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: Subhash C Thakur
+ */
+
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+/*
+ * Given a positive integer N, this function returns the longest consecutive
+ * zeros between two ones in the binary representation of N. That is length of
+ * maximal sequence of consecutive zeros with ones at both end.
+ */
+
+int binary_gap(int N);
+
+#endif /* UTILITIES_H_ */
