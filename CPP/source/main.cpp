a94a17470d1aa2c19ae6fb1bfa5f6489607277a7
Subht21 <subhash@lokawiz.com>
2016-02-15 22:53:45 +0530

    Deleting CPP folder for fresh errro free checkin.

--- CPP/source/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include <utils.hpp>
-
-int main ()
-{
-    int N = 40;
-    std::cout << "N: " <<N << std::endl;
-}
--- CPP/source/utils.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * utils.c
- *
- *  Created on: Feb 11, 2016
- *      Author: Subhash C Thakur
- */
-
