f30e67d54a2b3d9e37870f4badb77fd765c15bd6
Subht21 <subhash@lokawiz.com>
2016-02-15 21:10:40 +0530

    Adding CPP source and hader directories with inital files.

---
+++ CPP/source/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include <utils.hpp>
+
+int main ()
+{
+    int N = 40;
+    std::cout << "N: " <<N << std::endl;
+}
---
+++ CPP/source/utils.cpp
@@ -0,0 +1,7 @@
+/*
+ * utils.c
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: Subhash C Thakur
+ */
+
